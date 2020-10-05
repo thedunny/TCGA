3fba2262bb1b0b93639f47c509b0eaf52ce03763
Danielle <dani.danielletkm@gmail.com>
2020-10-05 05:26:36 -0300

    adicionando as funções

--- scripts/script1.R
+++ scripts/script1.R
@@ -1,37 +1,63 @@
+try_col <- function(files){
+  cont <- 0
+  for (f in files) {
+    tmp <- read.delim(f)
+    #lt[[paste0(f)]] <- tmp
+    if (tmp$bcr_patient_barcode){
+      cont <- cont+1
+    }
+  }
+  return (cont)
+}
+
+#tentar juntar new_col e add_col
+new_col <- function(files, cont){
+  if(cont == 65){
+    ndata <- data.frame(bcr_patient_barcode = factor())
+    for(f in files){
+      tmp <- read.delim(f)
+      ndata <-  rbind (ndata, select (tmp, bcr_patient_barcode))
+    }
+  }
+  return (ndata)
+}
+
+
+add_col <- function(ndata, dataf){
+    return( cbind (dataf, ndata))
+}
+
+
+
+
+
+
+
 files <- list.files(pattern="follow_up")
 
 setwd("C:/TCGA/Arquivos")
 
-getwd ()
-lt <- list()
-
-rm (ndata)
-ndata <- data.frame(bcr_patient_uuid = factor())
-
+#lt <- list()
 
 ndata ["bcr_patient_barcode"] <- factor()
-
-s_bcr <- 0
-for (f in files) {
-  tmp <- read.delim(f)
-  #lt[[paste0(f)]] <- tmp
-  if (tmp$bcr_patient_uuid){
-    s_bcr <- s_bcr+1
-  }
-}
-
-if(s_bcr == 65){
-  for(f in files){
-  tmp <- read.delim(f)
-  ndata <-  rbind (ndata, select (tmp, bcr_patient_uuid))
-  }
-}
-
-
-
-
-
+rm (bcr_patient_uuid)
 tmp <- lt[1]$nationwidechildrens.org_clinical_follow_up_v1.0_coad.txt
+dataff <- data.frame()
+ndataff <- data.frame()
+
+cont <- try_col(files) 
+#primeira vez
+dataff <- new_col(files, cont)
+#outras vezes
+ndataff <- new_col(files, cont)
+
+dataff <- add_col(ndataff, dataff)
+
+
+
+
+
+
 
 
 
