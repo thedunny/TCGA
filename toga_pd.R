044a65fe418f5d905ae60fe064fca40d39918b9e
dtiezzi <dtiezzi@usp.br>
2020-11-19 05:05:18 +0000

    pd dtiezzi 1

--- toga_pd.R
+++ toga_pd.R
@@ -4,15 +4,19 @@ setwd("/home/dtiezzi/Documents/Projetos/TCGA/")
 library(stringr)
 
 files <- paste('./Arquivos/', list.files('./Arquivos'), sep = '')
+
 cancer_types <- c()
 for (f in 1:length(files))
   cancer_types[f] <- str_sub(strsplit(files[f], '_')[[1]][-1][-1][-1],1,-5)
-cancer_types
 
 cancer_types_attibutes <- c()
 idx = 1
 for (f in files) {
-  tmp <- read.delim(files[1], skip = 1)[-1,]
+  tmp <- read.delim(f, skip = 1)[-1,]
+  nB <- apply(tmp, 2, grep, pattern='[[]')
+  nb <- lapply(nB, function(x) length(x)/nrow(tmp))
+  badCols <- names(nb)[nb > 0.5]
+  tmp <- tmp[, !colnames(tmp) %in% badCols]
   cancer_types_attibutes[cancer_types[idx]] <- list(colnames(tmp))
   idx <- idx + 1
 }
