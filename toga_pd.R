21bd610414c1c5b04e47793b383474307b2500ad
dtiezzi <dtiezzi@usp.br>
2020-11-20 10:27:02 +0000

    dtiezzi col select

--- toga_pd.R
+++ toga_pd.R
@@ -8,8 +8,9 @@ files <- paste('./Arquivos/', list.files('./Arquivos'), sep = '')
 cancer_types <- c()
 for (f in 1:length(files))
   cancer_types[f] <- str_sub(strsplit(files[f], '_')[[1]][-1][-1][-1],1,-5)
-cancer_types_filt <- cancer_types[c(2,3,4,5,6,8,10,16,17,18,20,21,23,24,27,29,31)]
-files <- files[c(2,3,4,5,6,8,10,16,17,18,20,21,23,24,27,29,31)]
+cancer_types
+cancer_types_filt <- cancer_types[c(2,3,4,5,6,8,12,13,17,20,21,23,24,27,29,31)]
+files <- files[c(2,3,4,5,6,8,12,13,17,20,21,23,24,27,29,31)]
 
 cancer_types_attibutes <- c()
 cancer_types_desc <- c()
@@ -40,3 +41,11 @@ for (cn in ColNames_filt) {
 
 cnInAll <- names(ColNames_count[ColNames_count == length(cancer_types_filt)])
 
+df_cn <- data.frame(n = sort(ColNames_count, decreasing = T))
+df_cn$Colnames <- rownames(df_cn)
+df_cn <- df_cn[, c(2,1)]
+rownames(df_cn) <- seq(1, nrow(df_cn))
+head(df_cn)
+df_cn
+sel_cols <- df_cn$Colnames[c(1:43,45,46,48:50,52:57, 63, 65, 172, 177, 295, 254)]
+sel_cols
